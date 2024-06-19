93a46f0a4974b6d06b0297761759d23fe2bf72eb
Athooh <s.athooh@gmail.com>
2024-06-19 19:28:20 +0300

    root path updated

--- main.go
+++ main.go
@@ -1,3 +1,24 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	handler "web/handlers"
+// )
+
+// func main() {
+// 	http.HandleFunc("/", handler.FormHandler)
+// 	http.HandleFunc("/ascii-art", handler.AsciiArtHandler)
+// 	http.HandleFunc("/favicon.ico", http.NotFound) // Handle favicon.ico requests
+// 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+// 	fmt.Println("Server started at http://localhost:8080")
+// 	err := http.ListenAndServe(":8080", nil)
+// 	if err != nil {
+// 		fmt.Println("Error starting server:", err)
+// 	}
+// }
+
 package main
 
 import (
