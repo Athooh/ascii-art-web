41025c5a43064d7c39fda1ffb407bb19f2aee66c
Athooh <s.athooh@gmail.com>
2024-06-19 20:19:47 +0300

    form interface updated

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,27 +1,4 @@
-    // // Handle form submission to support multiline input
-    // document.getElementById('asciiForm').onsubmit = function(e) {
-    //     e.preventDefault();
-    //     const text = document.getElementById('text').value;
-    //     const banner = document.getElementById('banner').value;
-    //     fetch('/ascii-art', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded',
-    //         },
-    //         body: new URLSearchParams({
-    //             text: text,
-    //             banner: banner,
-    //         }),
-    //     })
-    //     .then(response => response.text())
-    //     .then(data => {
-    //         document.open();
-    //         document.write(data);
-    //         document.close();
-    //     });
-    // };
-
-    // Handle form submission to support multiline input
+// Handle form submission to support multiline input
 document.getElementById('asciiForm').onsubmit = function(e) {
     e.preventDefault();
     const text = document.getElementById('text').value;
